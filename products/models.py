ddd921448b7bbfd3b43806589672dd0dbedbc8b0
Peyman Hassani <hassani.peyman627@gmail.com>
2021-08-26 16:57:10 +0430

    Create `products` app
    simple `Product` model was created for initial testing purposes

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.SlugField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f'name:{self.name} | price: {self.price}'
+
+
+class Category(models.Model):
+    pass
