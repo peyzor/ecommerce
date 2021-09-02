0c012a47d1763c7405a884a351c09f420fe266b0
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-02 11:16:36 +0430

    Add `ProductListView` and `ProductDetailView`

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
-    path('category-list',
+    path('category-list/',
          views.CategoryListView.as_view(),
          name='category_list'),
+    path('product-list/', views.ProductListView.as_view(),
+         name='product-list'),
+    path('product-detail/<int:pk>',
+         views.ProductDetailView.as_view(),
+         name='product-detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,21 @@
 from django.views import generic
 
-from .models import Category
+from .models import Category, Product
 
 
 class CategoryListView(generic.ListView):
     queryset = Category.objects.all()
     context_object_name = 'categories'
     template_name = 'products/category_list.html'
+
+
+class ProductListView(generic.ListView):
+    queryset = Product.objects.all()
+    context_object_name = 'products'
+    template_name = 'products/product_list.html'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'products/product_detail.html'
