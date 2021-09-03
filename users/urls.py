21d58f900386fe614057741ec357d5e349d47c5b
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-03 12:50:42 +0430

    Seperate view logic from urls.py file in users app

--- users/urls.py
+++ users/urls.py
@@ -1,31 +1,21 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/',
-         auth_views.LoginView.as_view(template_name='registration/login.html'),
-         name='login'),
-    path('logout/',
-         auth_views.LogoutView.as_view(
-             template_name='registration/logout.html'),
-         name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('password_reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/user_password_reset_form.html'),
+         views.PasswordResetView.as_view(),
          name='password_reset'),
     path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/user_password_reset_done.html'),
+         views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/user_password_reset_confirm.html'),
+         views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/user_password_reset_complete.html'),
+         views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-import django
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import views as auth_views
 
 
-def register(request):
+def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
 
@@ -20,3 +20,27 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+
+class LoginView(auth_views.LoginView):
+    template_name = 'registration/login.html'
+
+
+class LogoutView(auth_views.LogoutView):
+    template_name = 'registration/logout.html'
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    template_name = 'registration/user_password_reset_form.html'
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = 'registration/user_password_reset_done.html'
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = 'registration/user_password_reset_confirm.html'
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = 'registration/user_password_reset_complete.html'
