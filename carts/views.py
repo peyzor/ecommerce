1079077e14d7bd9fba1a8b92613eedcc0c26b4e7
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-04 17:57:35 +0430

    Fix current user can only see its own cart

--- carts/views.py
+++ carts/views.py
@@ -8,3 +8,8 @@ class CartDetailView(generic.DetailView):
     template_name = 'carts/cart_detail.html'
     context_object_name = 'cart'
     pk_url_kwarg = 'cart_id'
+
+    def get_queryset(self, *args, **kwargs):
+        """ only the current user can see its own cart """
+        qs = super().get_queryset(*args, **kwargs)
+        return qs.filter(user=self.request.user)
