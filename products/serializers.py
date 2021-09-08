d587d75ed00564f3611d57b3322a945d20597686
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-08 16:46:14 +0430

    Update `ProductSerializer` to be hyperlinked

--- products/serializers.py
+++ products/serializers.py
@@ -1,23 +1,28 @@
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 
 from .models import Product, Category
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='products:product_detail',
+        lookup_url_kwarg='product_id',
+        read_only=True)
     category = serializers.SlugRelatedField(slug_field='name', read_only=True)
 
     class Meta:
         model = Product
         fields = [
-            'id', 'name', 'price', 'category', 'image', 'created_time',
+            'url', 'id', 'name', 'price', 'category', 'image', 'created_time',
             'updated_time'
         ]
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(
-        view_name='products:category_detail', lookup_url_kwarg='category_id')
+        view_name='products:category_detail',
+        lookup_url_kwarg='category_id',
+        read_only=True)
     products = serializers.HyperlinkedRelatedField(
         view_name='products:product_detail',
         lookup_url_kwarg='product_id',
